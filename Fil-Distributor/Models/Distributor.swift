2b7e8904a11ebe7ec2999b3e4db05d6553630df7
hexcola <hexcola@gmail.com>
2023-10-20 23:40:19 +0800

    a prototype that support use secp256k1 private key to sign a message cid

--- Fil-Distributor/Models/Distributor.swift
+++ Fil-Distributor/Models/Distributor.swift
@@ -6,7 +6,101 @@
 //
 
 import Foundation
+import Blake2
+import secp256k1
 
+enum LotusError: Error {
+    case wrongMessageSize
+    
+    case wrongPrivateKeySize
+    
+    case invalidPrivateKey
+    
+    case signatureFailed
+}
+
+extension LotusError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .wrongMessageSize:
+            return "Message size must be 32"
+        case .wrongPrivateKeySize:
+            return "Private key size must be 32"
+        case .invalidPrivateKey:
+            return "Private key invalid"
+        case .signatureFailed:
+            return "Signature failed"
+        }
+    }
+}
+
+struct Lotus {
+    func cidToBytes(cid: String) -> Data? {
+        do {
+            let data = try Base32.decode(string: cid)
+            return data
+        } catch {
+            print("erro....")
+            return nil
+        }
+    }
+    
+    func pkToBytes(pk: String) -> Data?{
+        if let data = pk.data(using: .utf8) {
+            let byteArray = Data(base64Encoded: data)
+            return byteArray
+        } else {
+            print("faild to convert string to data")
+            return nil
+        }
+    }
+    
+    func blake2bSum256(msg:Data) -> Data? {
+        do {
+            let hash = try! Blake2b.hash(size: 32, data: msg)
+            return hash
+        } catch {
+            return nil
+        }
+    }
+    
+    func sigToString(sig:[UInt8]) -> String {
+        return Data(sig).base64EncodedString()
+    }
+    
+    func sign(msg:[UInt8], seckey:[UInt8]) throws -> [UInt8] {
+//        let context = secp256k1_context_create(1)!
+        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
+        
+        if msg.count != 32 {
+            throw LotusError.wrongMessageSize
+        }
+        
+        if seckey.count != 32 {
+            throw LotusError.wrongPrivateKeySize
+        }
+        
+        let seckeydata = UnsafeMutablePointer(mutating: seckey)
+        
+        if secp256k1_ec_seckey_verify(context, seckeydata) != 1 {
+            throw LotusError.invalidPrivateKey
+        }
+        
+        let msgdata = UnsafeMutablePointer(mutating: msg)
+        let noncefunc = secp256k1_nonce_function_rfc6979
+        var sigstruct = secp256k1_ecdsa_recoverable_signature()
+        
+        if secp256k1_ecdsa_sign_recoverable(context, &sigstruct, msgdata, seckeydata, noncefunc, nil) == 0 {
+            throw LotusError.signatureFailed
+            }
+
+            var sig = [UInt8](repeating: 0, count: 65)
+            var recid: Int32 = 0
+            secp256k1_ecdsa_recoverable_signature_serialize_compact(context, &sig, &recid, &sigstruct)
+            sig[64] = UInt8(recid) // add back recid to get 65 bytes sig
+            return sig
+    }
+}
 
 struct Message:Codable {
     var Version: Int
--- Fil-Distributor/Views/DistributorView.swift
+++ Fil-Distributor/Views/DistributorView.swift
@@ -15,10 +15,35 @@ struct DistributorView: View {
         
         Button(action: {
             print("Sign message for")
-            if (receivers != nil) {
-                let distributor = Distributor(fromAccount: fromAccount, receivers: receivers!)
-                distributor.send()
+//            if (receivers != nil) {
+//                let distributor = Distributor(fromAccount: fromAccount, receivers: receivers!)
+//                distributor.send()
+//            }
+            // TODO: remove the hardcode here.
+            let cid = "<Your own message cid without first letter>"
+            let pk = "<Your own secp256k1 private key>"
+            let l = Lotus()
+            if let msgData = l.cidToBytes(cid: cid.uppercased()) {
+//                let y = [UInt8](msgData)
+//                for byte in y {
+//                    print(byte)
+//                }
+                let b2sum = l.blake2bSum256(msg: msgData)!
+                if let pkData = l.pkToBytes(pk: pk) {
+                    do {
+                        let sig = try l.sign(msg: [UInt8](b2sum), seckey: [UInt8](pkData))
+                        print(l.sigToString(sig: sig))
+                    } catch {
+                        print(error)
+                        print("Can not get sig")
+                    }
+                } else {
+                    print("Can not get pkData")
+                }
+            } else {
+                print("something wrong")
             }
+            
         }, label: {
             Label("Send", systemImage: "paperplane")
         })
